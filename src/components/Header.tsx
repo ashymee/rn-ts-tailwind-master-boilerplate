82b2e0f527974636736ed665ca482eb2de4860bd
Aswin Himawan <ashymee@gmail.com>
2022-09-22 19:50:18 +0700

    add more space

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 const Header = () => {
   return (
-    <View className='bg-slate-900 p-2 rounded-2xl absolute top-10 left-5 right-5'>
+    <View className='bg-slate-900 p-2 rounded-2xl absolute top-14 left-5 right-5'>
       <Text className='text-orange-300 text-center'>
         React Native + TailwindCSS Master Boilerplate
       </Text>
